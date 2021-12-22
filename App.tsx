18ca856288f0257842ca50a5fef423fa8b094ece
obotha <oboth.abel.oa@gmail.com>
2021-12-22 15:20:17 +0300

    Create todos component.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,41 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+import Header from './components/header';
+import TodoItem from './components/todoItem';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'Buy coffee', key: '1' },
+    { text: 'Create an app', key: '2' },
+    { text: 'Play on the switch', key: '3' },
+  ]);
+
+  const pressHandler = (key: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key !== key);
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Header */}
+      <Header />
+      <View style={styles.content}>
+        {/* To do form */}
+
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+        </View>
+      </View>
+
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -14,7 +44,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
---
+++ components/todoItem.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, Text } from 'react-native';
+
+type TodoItemProps = {
+  item: { text: string; key: string };
+  pressHandler: (key: string) => void;
+};
+
+export default function TodoItem({ item, pressHandler }: TodoItemProps) {
+  return (
+    <TouchableOpacity onPress={() => pressHandler(item.key)}>
+      <Text style={styles.item}>{item.text}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 16,
+    marginTop: 16,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderRadius: 10,
+  },
+});
